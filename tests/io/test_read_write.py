f88fdc3ca9e4db8acb49c47c7363f749c2cf1570
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-12-05 14:29:14 +0100

    io test

---
+++ tests/io/test_read_write.py
@@ -0,0 +1,28 @@
+import pytest
+import torch
+
+from optical_flow.io.read_write import FORMATS, read, write
+
+
+@pytest.mark.parametrize("format", FORMATS)
+@pytest.mark.parametrize(
+    "device",
+    [
+        pytest.param(torch.device("cpu")),
+        pytest.param(
+            torch.device("cuda", 0),
+            marks=pytest.mark.skipif(
+                not torch.cuda.is_available(), reason="requires GPU"
+            ),
+        ),
+    ],
+)
+def test_read_write(tmpdir, format, device):
+    flow = torch.rand(2, 5, 6, device=device)
+    write(tmpdir / "test", flow, format=format)
+    loaded_flow = read(tmpdir / "test", format=format)
+    assert isinstance(loaded_flow, torch.Tensor)
+    assert loaded_flow.dtype == torch.float32
+    assert loaded_flow.shape == flow.shape
+    assert loaded_flow.device == torch.device("cpu")
+    assert torch.allclose(flow, loaded_flow)
