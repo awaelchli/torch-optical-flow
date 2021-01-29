0134475932746a227ace25ab67fdee4147606b68
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-01-29 05:13:25 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='torch_optical_flow',
+    version='0.1',
+    description='Optical Flow Utilities for PyTorch',
+    author='Adrian Wälchli',
+    author_email='aedu.waelchli@gmail.com',
+    url='',
+    packages=['optical_flow'],
+    install_requires=[
+        'torch',
+    ]
+)
