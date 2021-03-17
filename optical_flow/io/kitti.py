4add84168ec3922ec75c798dede5ee68b00f7cc7
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-03-17 04:30:02 +0100

    kitti io function

---
+++ optical_flow/io/kitti.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+
+def read_kitti(filename):
+    flow = cv2.imread(filename, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
+    flow = flow[:, :, ::-1].astype(np.float32)
+    flow, valid = flow[:, :, :2], flow[:, :, 2]
+    flow = (flow - 2 ** 15) / 64.0
+    return flow, valid
+
+
+def write_kitti(filename, uv):
+    uv = 64.0 * uv + 2 ** 15
+    valid = np.ones([uv.shape[0], uv.shape[1], 1])
+    uv = np.concatenate([uv, valid], axis=-1).astype(np.uint16)
+    cv2.imwrite(filename, uv[..., ::-1])
+
