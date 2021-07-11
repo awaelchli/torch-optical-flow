a4d27b4c4001747fa79dc45e18d2fcc210763ce9
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-11 19:43:54 +0200

    save

--- methods/raft/train.py
+++ methods/raft/train.py
@@ -13,36 +13,41 @@ import torch.optim as optim
 from data.datamodule import RAFTDataModule
 from model import RAFT
 from pytorch_lightning import seed_everything, Trainer
+from pytorch_lightning.utilities.cli import LightningCLI
 from pytorch_lightning.core import datamodule
 from torch.utils.data import DataLoader
 
 
+class RAFTCLI(LightningCLI):
+
+    def add_arguments_to_parser(self, parser):
+        parser.add_argument("--name", default="raft", help="name your experiment")
+        parser.add_argument("--restore_ckpt", help="restore checkpoint")
+        parser.add_argument("--validation", type=str, nargs="+")
+
+    def before_fit(self):
+        if self.restore_ckpt is not None:
+            self.model.load_state_dict(torch.load(self.restore_ckpt), strict=False)
+
+        if datamodule.stage != "chairs":
+            self.model.freeze_bn()
+
+
 def main():
     seed_everything(1234)
-    parser = ArgumentParser()
-    parser = Trainer.add_argparse_args(parser)
-    parser.add_argument("--name", default="raft", help="name your experiment")
-    parser.add_argument("--restore_ckpt", help="restore checkpoint")
-    parser.add_argument("--validation", type=str, nargs="+")
 
-    parser.set_defaults(
-        max_steps=100000,
-        gradient_clip_val=1.0,
-        validate_every_n_steps=5000,
+    cli = RAFTCLI(
+        RAFT,
+        RAFTDataModule,
+        description="RAFT",
+        trainer_defaults=dict(
+            max_steps=100000,
+            gradient_clip_val=1.0,
+            validate_every_n_steps=5000,
+        ),
     )
-    args = parser.parse_args()
 
-    model = RAFT()
-    datamodule = RAFTDataModule(root_chairs="/home/jovyan/optical-flow/FlyingChairs/data")
-
-    if args.restore_ckpt is not None:
-        model.load_state_dict(torch.load(args.restore_ckpt), strict=False)
-
-    if datamodule.stage != "chairs":
-        model.freeze_bn()
-
-    trainer = Trainer.from_argparse_args(args)
-    trainer.fit(model, datamodule=datamodule)
+    #datamodule = RAFTDataModule(root_chairs="/Volumes/Archive/Datasets/FlyingChairs/data")
 
 
 if __name__ == "__main__":
