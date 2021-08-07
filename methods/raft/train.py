e1318a324609672f110276eda5356ab472acdedb
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-08-07 14:33:53 +0200

    restore ckpt -> restore weights

--- methods/raft/train.py
+++ methods/raft/train.py
@@ -10,12 +10,13 @@ from pretrained.convert import strip_module
 
 class RAFTCLI(LightningCLI):
     def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
-        parser.add_argument("--restore_ckpt", help="restore checkpoint")
+        parser.add_argument("--restore_weights", help="restore checkpoint")
 
     def before_fit(self) -> None:
-        restore_path = self.config["restore_ckpt"]
+        restore_path = self.config["restore_weights"]
         if restore_path is not None:
-            state_dict = torch.load(restore_path)
+            checkpoint = torch.load(restore_path)
+            state_dict = checkpoint.get("state_dict", checkpoint)
             state_dict = strip_module(state_dict)
             self.model.load_state_dict(state_dict, strict=False)
             rank_zero_info(f"Restored weights from {restore_path}")
