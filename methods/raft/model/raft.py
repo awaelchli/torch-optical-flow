8638ec2ead6ebed54de60c357f4ad802ecc8df2c
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-08-08 01:28:08 +0200

    change tensor stacking and new loss function

--- methods/raft/model/raft.py
+++ methods/raft/model/raft.py
@@ -90,7 +90,7 @@ class RAFT(LightningModule):
         flow_init: Optional[Tensor] = None,
         upsample: bool = True,
         test_mode: bool = False,
-    ) -> Union[Tuple[Tensor, Tensor], List[Tensor]]:
+    ) -> Union[Tensor, Tuple[Tensor, Tensor]]:
         """Estimate optical flow between pair of frames"""
 
         image0 = 2 * (image0 / 255.0) - 1.0
@@ -142,6 +142,7 @@ class RAFT(LightningModule):
         if test_mode:
             return coords1 - coords0, flow_up
 
+        flow_predictions = torch.stack(flow_predictions)
         return flow_predictions
 
     def training_step(
@@ -225,68 +226,34 @@ class RAFT(LightningModule):
 
 
 def sequence_loss(
-    flow_preds: List[Tensor],
+    flow_preds: Tensor,
     flow_gt: Tensor,
     valid: Tensor,
     gamma: float = 0.8,
     max_flow: float = 400.0,
 ) -> Tuple[Tensor, Dict[str, float]]:
     """Loss function defined over sequence of flow predictions"""
-
     n_predictions = len(flow_preds)
-    flow_loss = 0.0
 
     # exclude invalid pixels and extremely large displacements
     mag = torch.sum(flow_gt ** 2, dim=1).sqrt()
     valid = (valid >= 0.5) & (mag < max_flow)
 
-    for i in range(n_predictions):
-        i_weight = gamma ** (n_predictions - i - 1)
-        i_loss = (flow_preds[i] - flow_gt).abs()
-        flow_loss += i_weight * (valid[:, None] * i_loss).mean()
+    idx = torch.arange(float(n_predictions), device=flow_gt.device)
+
+    # the weight for each element in the sequence
+    weight = gamma ** (n_predictions - idx - 1)
+
+    losses = valid[None, :, None] * (flow_preds - flow_gt[None]).abs()
+    flow_loss = torch.sum(weight * losses.flatten(1).mean(dim=1))
 
     epe = torch.sum((flow_preds[-1] - flow_gt) ** 2, dim=1).sqrt()
     epe = epe.view(-1)[valid.view(-1)]
 
     metrics = {
-        "epe": epe.mean().item(),
         "1px": (epe < 1).float().mean().item(),
         "3px": (epe < 3).float().mean().item(),
         "5px": (epe < 5).float().mean().item(),
     }
+
     return flow_loss, metrics
-
-
-# def sequence_loss(
-#     flow_preds: List[Tensor],
-#     flow_gt: Tensor,
-#     valid: Tensor,
-#     gamma: float = 0.8,
-#     max_flow: float = 400.0,
-# ):
-#     """Loss function defined over sequence of flow predictions"""
-#     flow_preds = torch.stack(flow_preds)
-#     n_predictions = len(flow_preds)
-
-#     # exclude invalid pixels and extremely large displacements
-#     mag = torch.sum(flow_gt ** 2, dim=1).sqrt()
-#     valid = (valid >= 0.5) & (mag < max_flow)
-
-#     idx = torch.arange(float(n_predictions), device=flow_gt.device)
-
-#     # the weight for each element in the sequence
-#     weight = gamma ** (n_predictions - idx - 1)
-
-#     losses = valid[None, :, None] * (flow_preds - flow_gt[None]).abs()
-#     flow_loss = torch.sum(weight * losses.flatten(1).mean(dim=1))
-
-#     epe = torch.sum((flow_preds[-1] - flow_gt) ** 2, dim=1).sqrt()
-#     epe = epe.view(-1)[valid.view(-1)]
-
-#     metrics = {
-#         "1px": (epe < 1).float().mean().item(),
-#         "3px": (epe < 3).float().mean().item(),
-#         "5px": (epe < 5).float().mean().item(),
-#     }
-
-#     return flow_loss, metrics
