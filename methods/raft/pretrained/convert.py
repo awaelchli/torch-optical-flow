8fe64c843ac9f037e55dbee200140c2aaa7d151d
awaelchli <>
2021-08-07 14:52:31 +0000

    fix bug in conversion

--- methods/raft/pretrained/convert.py
+++ methods/raft/pretrained/convert.py
@@ -7,6 +7,7 @@ def strip_module(state_dict: Dict[str, Any]) -> Dict[str, Any]:
     """ Strips the 'module.' prefix from the state-dict keys of a model checkpoint. """
     mapping = {old_key: old_key.replace("module.", "") for old_key in state_dict.keys()}
     for old_key, new_key in mapping.items():
-        state_dict[new_key] = state_dict[old_key]
-        del state_dict[old_key]
+        if new_key != old_key:
+            state_dict[new_key] = state_dict[old_key]
+            del state_dict[old_key]
     return state_dict
