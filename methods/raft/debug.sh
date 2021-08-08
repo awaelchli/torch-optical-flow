d3fb67c3580fdc546c41159c5cebae07d14f530e
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-08-08 19:00:27 +0200

    remove debug script

--- methods/raft/debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# python train.py --config config/chairs.yaml --trainer.gpus 1 --data.root_chairs ~/optical-flow-datasets/FlyingChairs/data
-python train.py --config config/things.yaml --trainer.gpus 1 --data.root_things ~/optical-flow-datasets/FlyingThings3D --data.root_sintel ~/optical-flow-datasets/MPI-Sintel  --restore_ckpt pretrained/models/raft-chairs.pth
