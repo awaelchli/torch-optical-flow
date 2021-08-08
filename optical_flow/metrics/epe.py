0f1902319bb0d8264880592c8deab04047f8a5ee
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-08-08 18:34:46 +0200

    add support for masking epe

--- optical_flow/metrics/epe.py
+++ optical_flow/metrics/epe.py
@@ -1,28 +1,39 @@
+from typing import Optional
+
 import torch
+from torch import Tensor
+
 from torchmetrics import Metric
 
 
 class AverageEndPointError(Metric):
-    """ Average End-to-end Point Error (AEPE) """
+    """Average End-to-end Point Error (AEPE)"""
 
-    def __init__(self, dim: int = 1):
+    def __init__(self, dim: int = 1) -> None:
         super().__init__()
         self.dim = dim
         self.add_state("sum_epe", default=torch.tensor(0.0), dist_reduce_fx="sum")
         self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
 
-    def update(self, pred: torch.Tensor, target: torch.Tensor):
-        epe = end_point_error(pred, target, dim=self.dim, reduce=False)
+    def update(
+        self, pred: Tensor, target: Tensor, valid: Optional[Tensor] = None
+    ) -> None:
+        epe = end_point_error(pred, target, dim=self.dim, reduce=False).view(-1)
+
+        if valid is not None:
+            valid = valid.view(-1) >= 0.5
+            epe = epe[valid]
+
         self.sum_epe += epe.sum()
         self.total += epe.numel()
 
-    def compute(self) -> torch.Tensor:
+    def compute(self) -> Tensor:
         return self.sum_epe / self.total
 
 
 def end_point_error(
-    pred: torch.Tensor, target: torch.Tensor, dim: int = 1, reduce: bool = True
-):
+    pred: Tensor, target: Tensor, dim: int = 1, reduce: bool = True
+) -> Tensor:
     epe = torch.norm(pred - target, p=2, dim=dim)
     if reduce:
         epe = epe.mean()
