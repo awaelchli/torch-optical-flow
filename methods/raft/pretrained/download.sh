4ec2841c1859309ac0be74c38682035fe3d02764
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-08-08 12:51:26 +0200

    update download script

--- methods/raft/pretrained/download.sh
+++ methods/raft/pretrained/download.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # our reproduced checkpoints
 wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/chairs-epoch%3D43-step%3D119999.ckpt -O checkpoints/raft-chairs.ckpt
+wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/things-epoch%3D7-step%3D119999.ckpt -O checkpoints/raft-things.ckpt
