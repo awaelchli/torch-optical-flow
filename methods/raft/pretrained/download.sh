ffe3eb0d5834e29bfccfc80722d13cc4946a971c
awaelchli <>
2021-08-09 19:23:20 +0000

    don't download if exists

--- methods/raft/pretrained/download.sh
+++ methods/raft/pretrained/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # our reproduced checkpoints
-wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/chairs-epoch%3D43-step%3D119999.ckpt -O checkpoints/raft-chairs.ckpt
-wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/things-epoch%3D7-step%3D119999.ckpt -O checkpoints/raft-things.ckpt
-wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/sintel-epoch%3D2-step%3D119999.ckpt -O checkpoints/raft-sintel.ckpt
-wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/kitti-epoch%3D1249-step%3D49999.ckpt -O checkpoints/raft-kitti.ckpt
+wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/chairs-epoch%3D43-step%3D119999.ckpt -nc -O checkpoints/raft-chairs.ckpt
+wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/things-epoch%3D7-step%3D119999.ckpt -nc -O checkpoints/raft-things.ckpt
+wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/sintel-epoch%3D2-step%3D119999.ckpt -nc -O checkpoints/raft-sintel.ckpt
+wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/kitti-epoch%3D1249-step%3D49999.ckpt -nc -O checkpoints/raft-kitti.ckpt
