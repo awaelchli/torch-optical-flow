fc0dc869dedd8f879c8f2184763302db518467c5
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-08-09 16:19:53 +0200

    add raft kitti checkpoint

--- methods/raft/pretrained/download.sh
+++ methods/raft/pretrained/download.sh
@@ -3,3 +3,4 @@
 wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/chairs-epoch%3D43-step%3D119999.ckpt -O checkpoints/raft-chairs.ckpt
 wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/things-epoch%3D7-step%3D119999.ckpt -O checkpoints/raft-things.ckpt
 wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/sintel-epoch%3D2-step%3D119999.ckpt -O checkpoints/raft-sintel.ckpt
+wget https://torch-optical-flow.s3.eu-central-1.amazonaws.com/checkpoints/raft/kitti-epoch%3D1249-step%3D49999.ckpt -O checkpoints/raft-kitti.ckpt
