66ea64999d84b272f8a1aa670bf280af1f36d666
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-08-15 00:53:47 +0200

    x

--- methods/raft/data/dataset.py
+++ methods/raft/data/dataset.py
@@ -244,12 +244,12 @@ class HD1K(FlowDataset):
             seq_ix += 1
 
 
-def read_gen(file_name):
-    ext = os.path.splitext(file_name)[-1]
+def read_gen(filename):
+    ext = os.path.splitext(filename)[-1]
     if ext == ".png" or ext == ".jpeg" or ext == ".ppm" or ext == ".jpg":
-        return Image.open(file_name)
+        return Image.open(filename)
     elif ext == ".flo":
-        return optical_flow.read(file_name, fmt="middlebury")
+        return optical_flow.read(filename, fmt="middlebury")
     elif ext == ".pfm":
-        return optical_flow.read(file_name, fmt="pfm")
+        return optical_flow.read(filename, fmt="pfm")
     return []
